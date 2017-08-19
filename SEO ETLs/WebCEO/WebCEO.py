9778fe09d3a02815b14fe7574a49c5a78a438f74
Carter Fritz <carter.fritz@utexas.edu>
2017-08-19 10:54:04 -0500

    matching master repo. I couldn't git fetch and overwrite local successfully, plus i duno if it would have killed any local files that i needed like config/credentials

--- SEO ETLs/WebCEO/WebCEO.py
+++ SEO ETLs/WebCEO/WebCEO.py
@@ -32,6 +32,10 @@ import requests
 #import urllib2 # DNE in python3?
 import json
 import webbrowser
+import csv
+import sys
+
+
 
 # API command to get the list of projects added to a user's account in Web CEO
 
@@ -50,11 +54,50 @@ print(payload_dict)
 payload = json.dumps(payload_dict)
 print(payload)
 
-r = requests.post(url, data=payload)
-print(r.text)
-#r = requests.get(url, params=payload)
+result = requests.post(url, data=payload)
+print(result.text)
+#r = requests.get(url, params=payload) # i duno what this does
 
-#with open("requests_results.html","wb") as f:
-#    f.write(r.content)
-#webbrowser.open("results.html")
+"""
+# This writes to a .json or .txt depending on how the file extension. Right now this is a .json file but filled with "result.text". I have no idea if this would be usable as a json. I think so since the text editor recognizes it.
+handle = open("test.json", "w")
+handle.write(result.text)
+handle.close()
+"""
 
+#TODO: Learn how to write to a csv.
+# Everything below here is garbage.
+result_parsed = json.loads(result)
+print(result_parsed)
+
+#'data' is the first term in the json file. eg: [{"data":[{"project":"3cc5dac7"...
+
+result_data = result_parsed['data']
+
+# open a file for writing
+
+json_data = open('json_data.csv', 'w')
+
+# create the csv writer object
+
+csvwriter = csv.writer(json_data)
+
+count = 0
+
+for emp in json_data:
+      if count == 0:
+             header = emp.keys()
+             csvwriter.writerow(header)
+             count += 1
+      csvwriter.writerow(emp.values())
+json_data.close()
+#TODO: Learn how to write to a tsv.
+"""
+# Don't know if I can use any of this below here.
+with open('output.tsv', 'w') as output_file:
+    dw = csv.DictWriter(output_file, sorted(result[0].keys()), delimiter='\t')
+    dw.writeheader()
+    dw.writerows(result)
+"""
+#TODO: Learn how to write json format data into csv.
+#TODO: Learn how to write json and csv data into SQL
