c84f081b7e374804010a5233d9adbf236221db14
carter.fritz@utexas.edu <Revevoc8192!>
2017-08-20 16:33:42 -0500

    1. Found out how to get the "data" section of the JSON output. 2. Can now write the relevant JSON data to csv. 3. Established multiple ways to write original JSON data to .txt and .json files.
    TODO:
    1. Write to tsv.
    2. Actually learn enough of WebCEO to get meaningful data instead of just project details.

--- SEO ETLs/WebCEO/WebCEO.py
+++ SEO ETLs/WebCEO/WebCEO.py
@@ -37,6 +37,7 @@ import sys
 
 
 
+
 # API command to get the list of projects added to a user's account in Web CEO
 
 url = "https://online.webceo.com/api/"
@@ -50,48 +51,76 @@ payload_dict = {
     'key': api_key2,
     'method': 'get_projects'
 }
-print(payload_dict)
+#print(payload_dict) # prints as a single quoted payload
 payload = json.dumps(payload_dict)
-print(payload)
+#print(payload) # prints as a double quoted payload
 
 result = requests.post(url, data=payload)
-print(result.text)
-#r = requests.get(url, params=payload) # i duno what this does
+#print(result.text) # Returns a working JSON, but as a list. with double quotes
+#print(result.json()) # Returns a working JSON, but as a list. with single quotes
+#print(result.content) # THIS RETURNS BINARY DATA. CAN I USE IT?
+
+
+
 
 """
-# This writes to a .json or .txt depending on how the file extension. Right now this is a .json file but filled with "result.text". I have no idea if this would be usable as a json. I think so since the text editor recognizes it.
-handle = open("test.json", "w")
+# This writes to a .json or .txt depending on how the file extension.
+# JSONlint.com recognizes all of these as valid JSON.
+# Option 1. Prints on one line.
+handle = open("data.json", "w")
 handle.write(result.text)
 handle.close()
+
+# Option 2 Prints on one line.
+with open("data2.json", "wb") as f:
+    f.write(result.content)
 """
 
-#TODO: Learn how to write to a csv.
-# Everything below here is garbage.
-result_parsed = json.loads(result)
-print(result_parsed)
+
+
+#############################################################
+# How to extract relevant JSON "data" and write to a CSV.
+# First lines show how to indent JSON output.
+#############################################################
+# This block establishes the server response in a pretty format.
+result_parsed = json.loads(result.content)
+#print(json.dumps(result_parsed, indent=4)) # This is a test print of JSON data in a pretty format. It can only be written as "w", not "wb".
+
+"""
+# Option 3 for printing to a file as a PRETTY JSON 
+with open("data3.json", "w") as f:
+    f.write(json.dumps(result_parsed, indent=2))
+"""
 
 #'data' is the first term in the json file. eg: [{"data":[{"project":"3cc5dac7"...
+#result_data = result_parsed["data"] #This data is apparently a LIST with only 1 value.
+"""
+json1_data = result_parsed[0] # Get the first value of the array.
+data = json1_data['data'] #Now I can specify to just a particular object. In this test case: 'data'.
+#print(data) #printing results to confirm. 
+"""
+json2_data = result_parsed[0]['data'] # A condensed form of the block directly above.
+#print(json2_data)
 
-result_data = result_parsed['data']
 
-# open a file for writing
+"""
+json_to_csv = open('json_data.csv', 'w') # open a file for writing
+csvwriter = csv.writer(json_to_csv)# create the csv writer object
+count = 0 #start at 0
 
-json_data = open('json_data.csv', 'w')
-
-# create the csv writer object
-
-csvwriter = csv.writer(json_data)
-
-count = 0
-
-for emp in json_data:
+for data in json2_data:
       if count == 0:
-             header = emp.keys()
+             header = data.keys()
              csvwriter.writerow(header)
              count += 1
-      csvwriter.writerow(emp.values())
-json_data.close()
+      csvwriter.writerow(data.values())
+json_to_csv.close()
+#output = csv.DictWriter(open('json_data.csv','w'), delimiter=',', lineterminator='\n', fieldnames=['project', 'domain', 'name']) # trying to kill blank lines in csv.
+print("         bottom of file") # my place marker in console to make sure everything executed in new code additions.
+"""
+#############################################################
 #TODO: Learn how to write to a tsv.
+#############################################################
 """
 # Don't know if I can use any of this below here.
 with open('output.tsv', 'w') as output_file:
@@ -99,5 +128,7 @@ with open('output.tsv', 'w') as output_file:
     dw.writeheader()
     dw.writerows(result)
 """
-#TODO: Learn how to write json format data into csv.
-#TODO: Learn how to write json and csv data into SQL
+
+#############################################################
+#TODO: Learn how to write JSON and csv data into SQL via SQLalchemy or some other option.
+#############################################################
